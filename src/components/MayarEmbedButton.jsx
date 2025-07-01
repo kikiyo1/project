10a003746ffda44a05064bc6c926108f185c1f5b
kikiyo1 <bocahbagus168@gmail.com>
2025-07-01 11:41:17 +0700

    Update MayarEmbedButton.jsx

--- src/components/MayarEmbedButton.jsx
+++ src/components/MayarEmbedButton.jsx
@@ -2,22 +2,27 @@ import React, { useEffect } from "react";
 
 const MayarEmbedButton = () => {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://mayarembed.r2.mayar.id/mayar-new-min.js";
-    script.async = true;
-    document.body.appendChild(script);
+    const existing = document.querySelector("script[src='https://mayarembed.r2.mayar.id/mayar-new-min.js']");
+    if (!existing) {
+      const script = document.createElement("script");
+      script.src = "https://mayarembed.r2.mayar.id/mayar-new-min.js";
+      script.async = true;
+      document.body.appendChild(script);
+    }
   }, []);
 
   return (
-    <a
-      className="mayar-button iframe-lightbox-link"
-      href="https://sisworo-74397.myr.id/pl/aplikasi-kasir-hd-solution?iframe=true"
-      data-padding-bottom="30%"
-      data-scrolling="true"
-    >
-      <span></span>
-      Beli Sekarang
-    </a>
+    <div className="text-center mt-6">
+      <a
+        className="mayar-button iframe-lightbox-link"
+        href="https://sisworo-74397.myr.id/pl/aplikasi-kasir-hd-solution?iframe=true"
+        data-padding-bottom="30%"
+        data-scrolling="true"
+      >
+        <span></span>
+        Beli Sekarang
+      </a>
+    </div>
   );
 };
 
